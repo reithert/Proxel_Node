78e1480f9f91de60768ed1cd57d09bea1dcd6f89
reithert2424 <reithert2424@uwec.edu>
2022-02-22 21:28:00 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+
+Console.WriteLine("Enter Command: ");
+string command = Console.ReadLine();
+Console.WriteLine(command);
+
+using (Process process = new Process()) {
+    process.StartInfo.FileName = "powershell.exe";
+    process.StartInfo.Arguments = command;
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.RedirectStandardOutput = true;
+    process.Start();
+    
+    string output = process.StandardOutput.ReadToEnd();
+    File.WriteAllText("output.txt", output);
+}
